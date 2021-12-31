e5436685e0bd92982fbb2412ad6f3802b84849d9
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2021-12-31 19:17:45 +0800

    Added eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">
-        Work in progress
-      </h1>
+      <h1 className="text-3xl font-bold underline">Work in progress</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
