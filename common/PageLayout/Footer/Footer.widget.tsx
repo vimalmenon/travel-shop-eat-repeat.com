f7af1052244ea01c7d266e4960d8a03c4ad910eb
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-01-15 20:15:49 +0800

    Added CacheProvider

--- common/PageLayout/Footer/Footer.widget.tsx
+++ common/PageLayout/Footer/Footer.widget.tsx
@@ -1,10 +1,25 @@
-import { useFooterHelper } from '@store';
+import { RootState, useFooterHelper } from '@store';
+import { IMetaData } from '@types';
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
 export const Footer: React.FC = () => {
   const { versionNumber } = useFooterHelper();
+  const data = useSelector<RootState, Record<string, IMetaData[]>>(
+    (state) => state.app.navigationList
+  );
+  const navigation = data['FooterNavigation'] || [];
   return (
     <footer className="flex text-sm py-1 px-2 justify-between h-12 items-center">
-      <span></span>
+      <span>
+        {navigation.map((nav, key) => {
+          return (
+            <Link href={nav.url} key={key}>
+              <a>{nav.name}</a>
+            </Link>
+          );
+        })}
+      </span>
       <span>{versionNumber}</span>
     </footer>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
 import '../styles/globals.css';
 
+import { CacheProvider, EmotionCache } from '@emotion/react';
 import { store } from '@store';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+import createEmotionCache from '../config/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp: React.FC<MyAppProps> = ({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <CacheProvider value={emotionCache}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </CacheProvider>
   );
 };
 
